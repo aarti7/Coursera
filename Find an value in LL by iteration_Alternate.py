8e8bb74a2b27736314bf29abd843b41ef8d7f2dc
aarti7 <aarti.s2014@gmail.com>
2016-05-21 04:01:49 -0700

    Update and rename Find an value in LL by iteration.py to Find an value in LL by iteration_Alternate.py

--- Find an value in LL by iteration_Alternate.py
+++ Find an value in LL by iteration_Alternate.py
@@ -1,30 +1,26 @@
 #### Find an value in LL by iteration####
-###http://www.codeskulptor.org/#user40_3i4PzDsfdR_3.py
+###http://www.codeskulptor.org/#user41_3N38bL2d4sP2MKw_0.py
 class Node:
     def __init__(self, value):
         self.datum = value  											
-        self.n = None  										
-
+        self.next = None  										
 class LinkedLis:
     def __init__(self):
         self.head = None 											
            
     def push(self, newdata): 									
         addednode = Node(newdata) 								
-        addednode.n = self.head 								
+        addednode.next = self.head 								
         self.head = addednode										
 
 def find(head, valuetofind):
     temp = head
-
     while(temp):
         if temp.datum == valuetofind:
-            #print True
             return True
-
         else:
-            print valuetofind, "doesn't match", temp.datum
-            temp = temp.n
+            #print valuetofind, "doesn't match", temp.datum
+            temp = temp.next
     return False          
             
 if __name__=='__main__':
@@ -32,4 +28,4 @@ if __name__=='__main__':
     L.push(3)
     L.push(34)
     L.push(134)
-    print find(L.head.n,3)    
+    print find(L.head,31)   
